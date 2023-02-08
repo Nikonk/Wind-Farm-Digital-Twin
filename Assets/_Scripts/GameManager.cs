663b6f701ca86f2070c890eebaaaf9c5d1bce332
Nikonk <niksha14@mail.ru>
2023-02-08 15:24:40 +0800

    Day and Night cycle

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // public GameParameters gameParameters;
+
     private void Awake() 
     {
         DataHandler.LoadGameData();
+        // GetComponent<DayAndNightCycler>().enabled = gameParameters.enableDayAndNightCycle;
     }
 }
